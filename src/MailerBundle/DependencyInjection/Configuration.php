76b29468f4f1723554147542169f094775342ada
Konstantin.Myakshin <koc-dp@yandex.ru>
2017-08-11 02:16:40 +0300

    Start writing configuration for bundle

--- src/MailerBundle/DependencyInjection/Configuration.php
+++ src/MailerBundle/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Brouzie\MailerBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -14,13 +15,67 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->scalarNode('sender')
-                ->defaultNull()
-                ->info('Service name of the cache pool.')
-                ->example('cache.app')
+                ->append($this->getSenderNode())
             ->end()
         ;
 
+//        $this->addEmailsSection($rootNode);
+
         return $treeBuilder;
+    }
+
+    private function getSenderNode()
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('sender');
+
+        $node
+            ->isRequired()
+            ->beforeNormalization()
+            ->ifString()
+                ->then(function($value) { return ['address' => $value]; })
+            ->end()
+            ->beforeNormalization()
+            ->ifArray()
+                ->then(function($value) {
+                    if (!isset($value['address']) && reset($value)) {
+                        return ['address' => key($value), 'name' => reset($value)];
+                    }
+
+                    return $value;
+                })
+            ->end()
+            ->children()
+                ->scalarNode('name')->end()
+                ->scalarNode('address')->isRequired()->cannotBeEmpty()->end()
+            ->end();
+
+        return $node;
+    }
+
+    private function addEmailsSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->fixXmlConfig('email')
+            ->children()
+                ->arrayNode('emails')
+                    ->useAttributeAsKey('code')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('subject')->end()
+                            ->scalarNode('body')->end()
+                            ->scalarNode('body_html')->end()
+                            ->scalarNode('template')->end()
+                            ->fixXmlConfig('required_context_key')
+                            ->arrayNode('required_context_keys')
+                                ->defaultValue([])
+                                ->prototype('scalar')->end()
+                            ->end()
+                            ->append($this->getSenderNode())
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
     }
 }
--- src/MailerBundle/Tests/DependencyInjection/ConfigurationTest.php
+++ src/MailerBundle/Tests/DependencyInjection/ConfigurationTest.php
@@ -2,7 +2,63 @@
 
 namespace Brouzie\MailerBundle\Tests\DependencyInjection;
 
-class ConfigurationTest
-{
+use Brouzie\MailerBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Processor;
 
+class ConfigurationTest extends TestCase
+{
+    /**
+     * @dataProvider senderDataProvider
+     */
+    public function testSenderNodeNormalization(array $userConfig, array $normalizedConfig)
+    {
+        $processor = new Processor();
+        $config = $processor->processConfiguration(new Configuration(), [$userConfig]);
+
+        $this->assertEquals($normalizedConfig, $config);
+    }
+
+    /**
+     * @dataProvider invalidSenderProvider
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testSenderNodeRequired(array $userConfig)
+    {
+        $processor = new Processor();
+        $config = $processor->processConfiguration(new Configuration(), [$userConfig]);
+    }
+
+    public function senderDataProvider()
+    {
+        return [
+            [
+                ['sender' => 'info@site.com'],
+                ['sender' => ['address' => 'info@site.com']],
+            ],
+            [
+                ['sender' => ['info@site.com' => 'Site Notifications System']],
+                ['sender' => ['address' => 'info@site.com', 'name' => 'Site Notifications System']],
+            ],
+            [
+                ['sender' => ['address' => 'info@site.com', 'name' => 'Site Notifications System']],
+                ['sender' => ['address' => 'info@site.com', 'name' => 'Site Notifications System']],
+            ],
+        ];
+    }
+
+    public function invalidSenderProvider()
+    {
+        return [
+            [
+                [],
+            ],
+            [
+                ['sender' => null],
+            ],
+            [
+                ['sender' => ['address' => null]],
+            ],
+        ];
+    }
 }
